6061f92d87c540644f2ef06973df41581b9f4648
Leo Castilho <leonardosiqueira@gea.inatel.br>
2016-06-16 13:29:57 -0700

    Starting files

---
+++ obstacle.cpp
@@ -0,0 +1,24 @@
+/*-----------------------------------------------*/
+/*  obstacle.cpp                                 */
+/*  Obstacle class. Receive a list of vertices   */
+/*  Leonardo Castilho                            */
+/*  Start: June 15th                             */
+/*-----------------------------------------------*/
+
+#include "point.h"
+#include "obstacle.h"
+
+  Obstacle::~Obstacle(){
+    Point* deleting;
+    while(vertices_.size() != 0){
+      deleting = vertices_[vertices_.size()-1];
+      delete deleting;
+      vertices_.pop_back();
+    }
+  }
+  
+  int Obstacle::get_num_vert(){
+    return vertices_.size();
+  }
+
+
