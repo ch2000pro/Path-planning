571d97c7812c0f1eec78269473ea0c258d51b39d
feisabel <fernandampi@yahoo.com.br>
2016-07-15 12:26:32 -0400

    line sweep updated

--- point.h
+++ point.h
@@ -26,9 +26,9 @@ public:
         x = my_x; y = my_y; z = my_z; seg1 = 0; seg2 = 0;
     }
     
-    bool operator < (Point* p) const {
+    /*bool operator < (Point* p) const {
         return (x < p->getX() || (x == p->getX() && y > p->getY()));
-    }
+    }*/
     
     void setX(double x_){
         x = x_;
